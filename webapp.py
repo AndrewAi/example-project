6aaaf6f61ee8b91bb72639cb9f89e6c134ce78ee
Ian McLoughlin <Ian.McLoughlin@gmit.ie>
2016-10-17 09:51:47 +0100

    Added AJAX

--- webapp.py
+++ webapp.py
@@ -5,15 +5,18 @@
 #   http://flask.pocoo.org/
 
 import flask as fl
+import itertools as it
+
 app = fl.Flask(__name__)
 
 @app.route("/")
 def root():
     return app.send_static_file('index.html')
 
-@app.route("/name", methods=["GET", "POST"])
-def name():
-    return "Hello " + fl.request.form["name"] + "!"
+@app.route("/perms")
+def perms():
+	perms = [''.join(p) for p in it.permutations(fl.request.args.get("userinput")]
+	return '\n'.join(perms)
 
 if __name__ == "__main__":
     app.run()
